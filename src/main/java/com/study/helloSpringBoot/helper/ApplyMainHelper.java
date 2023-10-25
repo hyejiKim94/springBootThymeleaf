e33a69d0b12c2cad14cb8dbf4677e5c40078520c
hyeji.kim <kim.hyeji@systemi.co.jp>
2023-10-26 02:11:49 +0900

    Skip some of data setting

--- src/main/java/com/study/helloSpringBoot/helper/ApplyMainHelper.java
+++ src/main/java/com/study/helloSpringBoot/helper/ApplyMainHelper.java
@@ -3,11 +3,39 @@ package com.study.helloSpringBoot.helper;
 import com.study.helloSpringBoot.dto.apply.ApplyMainDto;
 import com.study.helloSpringBoot.vo.EmployeeBasicInfoVo;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
 public class ApplyMainHelper {
 
+  /**
+   * 初期画面表示設定
+   * @param employeeInfo - 社員情報
+   * @return
+   */
   public ApplyMainDto initApplyMainDto(EmployeeBasicInfoVo employeeInfo) {
     ApplyMainDto mainDto = new ApplyMainDto();
+    // Header 社員情報
     mainDto.setEmployeeVo(employeeInfo);
+    // Search area
+    mainDto.setYearList(this.getDefaultYearList());
     return mainDto;
   }
+
+  /**
+   * セレクトボックスリスト設定 - 申請年度
+   * @return
+   */
+  private List<Integer> getDefaultYearList() {
+    Calendar cal = Calendar.getInstance();
+    int currentYear = cal.get(Calendar.YEAR);
+    List<Integer> yearList = new ArrayList<>();
+    for (int i = 0; i < 3; i++) {
+      yearList.add(currentYear - i);
+    }
+    return yearList;
+  }
+
+
 }
